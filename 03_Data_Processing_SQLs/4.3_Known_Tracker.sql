b0fd3810451e6b8d0f7076114bc7c82ebea4f490
ChrisBeeti <32492572+ChrisBeeti@users.noreply.github.com>
2025-01-27 14:17:40 +0100

    Update 4.3_Known_Tracker.sql

--- 03_Data_Processing_SQLs/4.3_Known_Tracker.sql
+++ 03_Data_Processing_SQLs/4.3_Known_Tracker.sql
@@ -1,2 +1,2 @@
 # Known Tracker
-SELECT browser_id, is_tracker, count(*) FROM measurement.requests GROUP BY browser_id, is_tracker
+SELECT browser_id, is_tracker, count(*) FROM filterlists.measurement.requests GROUP BY browser_id, is_tracker
